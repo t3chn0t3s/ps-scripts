328b72e1628ece48ea3a2425b0e23f1838cf56d5
t3chn0t3s <46801776+t3chn0t3s@users.noreply.github.com>
2021-01-20 14:00:16 -0600

    Create New File
    I had remove this function from the deployDC.ps1 script and use it in a separate file as the reboot interrupted the script.

---
+++ configureNetworking.ps1
@@ -0,0 +1,14 @@
+# Simple script to make it easier to configure the intial networking on a Windows hosts
+
+$IPAddress = Read-Host "What IP address should be used?"
+$PrefixLength = Read-Host "What prefix length should be used?"
+$DefaultGateway = Read-Host "What default gateway should be used?"
+$ComputerName = Read-Host "What computer name should be used?"
+
+# Set DC's IP address and DNS server
+New-NetIPAddress -InterfaceAlias "Ethernet0"  -IPAddress $IPAddress -PrefixLength $PrefixLength -DefaultGateway $DefaultGateway
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddress $IPAddress
+
+# Set DC's computer name
+Rename-Computer -NewName $ComputerName
+
