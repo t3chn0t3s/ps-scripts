16f9a4c7c51f290fb1fa6cc9870ea82e6234d360
t3chn0t3s <46801776+t3chn0t3s@users.noreply.github.com>
2021-01-20 22:03:02 -0600

    Update configureNetworking.ps1
    I added a variable for $DNSServer

--- configureNetworking.ps1
+++ configureNetworking.ps1
@@ -3,11 +3,12 @@
 $IPAddress = Read-Host "What IP address should be used?"
 $PrefixLength = Read-Host "What prefix length should be used?"
 $DefaultGateway = Read-Host "What default gateway should be used?"
+$DNSServer = Read-Host "What is the IP of the DNS server?"
 $ComputerName = Read-Host "What computer name should be used?"
 
 # Set DC's IP address and DNS server
 New-NetIPAddress -InterfaceAlias "Ethernet0"  -IPAddress $IPAddress -PrefixLength $PrefixLength -DefaultGateway $DefaultGateway
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddress $IPAddress
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddress $DNSServer
 
 # Set DC's computer name
 Rename-Computer -NewName $ComputerName
